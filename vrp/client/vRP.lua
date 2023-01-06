722a1f469d0d438ae687dd79ba17471bd5b24b5a
Boss Man <joshualicht18@gmail.com>
2023-01-06 16:03:19 -0600

    Initial Commit

---
+++ vrp/client/vRP.lua
@@ -0,0 +1,19 @@
+-- https://github.com/ImagicTheCat/vRP
+-- MIT license (see LICENSE or vrp/vRPShared.lua)
+
+
+local vRPShared = module("vrp", "vRPShared")
+
+-- Client vRP
+local vRP = class("vRP", vRPShared)
+
+function vRP:__construct()
+  vRPShared.__construct(self)
+
+  -- load config
+  self.cfg = module("vrp", "cfg/client")
+  
+  TriggerServerEvent("vRPcli:playerSpawned")	-- triggers player reload
+end
+
+return vRP
